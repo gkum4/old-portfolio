3d87ae39cb18753e7b5a1aca756682e816172908
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2020-09-23 14:17:41 -0300

    Up portfolio

---
+++ precache-manifest.23d36ab9c3477e5a3a40ae2c6f7c3fa5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19eb206dbbbfb5e31a37591d0315d2d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60f47581b97f68883353",
+    "url": "/static/js/2.d308087a.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/static/js/2.d308087a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03be09512b4e693716b0",
+    "url": "/static/js/main.33b88d1b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "d46d7a246bbb67ead688efc1e825f4fd",
+    "url": "/static/media/alarmbear.d46d7a24.png"
+  },
+  {
+    "revision": "9ae4de4d6b744606028f763edeb2f120",
+    "url": "/static/media/gambiarrakeyboard.9ae4de4d.jpg"
+  },
+  {
+    "revision": "a1f2b376a6ff02ddc70af23ba8fb345a",
+    "url": "/static/media/gobarber-display.a1f2b376.png"
+  },
+  {
+    "revision": "e576ee1998bd25f387fcb9ae34352524",
+    "url": "/static/media/me-image.e576ee19.jpg"
+  },
+  {
+    "revision": "23d6b9ad650e92ed98527fe00c209ac7",
+    "url": "/static/media/places2go.23d6b9ad.png"
+  },
+  {
+    "revision": "5d3c2e18c2be79f08ffd2352ca2f41f1",
+    "url": "/static/media/skip.5d3c2e18.png"
+  },
+  {
+    "revision": "fa881cca9114ade1349caf21ff462ced",
+    "url": "/static/media/songlyrics.fa881cca.png"
+  }
+]);
